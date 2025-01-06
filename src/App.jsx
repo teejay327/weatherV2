cbfec48c28502eb8df76e91d4b73dae9771c2a41
teejay327 <mrenergy@hotmail.com>
2025-01-06 17:13:20 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">WeatherLink</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SIdebar.jsx
@@ -0,0 +1,12 @@
+
+
+const Sidebar = () => {
+
+  return (
+    <aside className="w-1/3 px8--8 py-16 bg-stone-900 text-stone-200 md:w-72 rounded-r-xl">
+
+    </aside>
+  )
+};
+
+export default Sidebar;
