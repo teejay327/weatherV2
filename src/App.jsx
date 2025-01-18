1f3bd34a3dd5323e035b292ca7202e0a7def436c
teejay327 <mrenergy@hotmail.com>
2025-01-18 14:14:12 +1000

    Added Router functionality and Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import NavBar from "./components/UI/NavBar.jsx";
-import Sidebar from "./components/UI/Sidebar.jsx";
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
 
+// import NavBar from "./components/UI/NavBar.jsx";
+import Sidebar from "./components/UI/Sidebar.jsx";
+import Home from './pages/Home.jsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home />}
+]);
 
 const App = () => {
   return (
     <div>
       {/* <NavBar /> */}
-    
+      <RouterProvider router={router} />
       <div className="flex">
         <Sidebar className="justify-start"/>
         <h1 className="h-screen w-screen text-center text-5xl font-bold bg-weather-teal text-teal-500"><span className="text-amber-400">Weather</span>Link</h1>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+
+const Home = () => {
+
+  return (
+    <h1>Weather forecast for your area</h1>
+  )
+}
+
+export default Home;
