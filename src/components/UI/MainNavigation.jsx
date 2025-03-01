ac3fdbf359ba872ae8e9fd77995b989bc1e2f1f3
teejay327 <mrenergy@hotmail.com>
2025-03-01 23:04:48 +1000

    Changed Menu icon to right and better contrast for link text v background

--- src/components/UI/MainNavigation.jsx
+++ src/components/UI/MainNavigation.jsx
@@ -6,13 +6,13 @@ const MainNavigation = () => {
     <nav className="flex">
 
         <ul className="flex space-x-6">
-          <li className="bg-stone-900 text-slate-200 px-4 py-2 rounded-md hover:text-yellow-500">
-            <NavLink to="/" className={({isActive}) => isActive ? "text-slate-400 hover:bg-slate-700" : undefined} end>
+          <li className="bg-stone-900 text-slate-200 px-4 py-2 rounded-md hover:text-yellow-700">
+            <NavLink to="/" className={({isActive}) => isActive ? "text-yellow-400 hover:bg-slate-700" : undefined} end>
               Home
             </NavLink>
           </li>
-          <li className="bg-stone-900 text-slate-200 px-4 py-2 rounded-md hover:text-yellow-500">
-            <NavLink to="/search" className={({isActive}) => isActive ? "text-slate-400 hover:bg-slate-700" : undefined}>
+          <li className="bg-stone-900 text-slate-200 px-4 py-2 rounded-md hover:text-yellow-700">
+            <NavLink to="/search" className={({isActive}) => isActive ? "text-yellow-400 hover:bg-slate-700" : undefined}>
               Search
             </NavLink>
           </li>
