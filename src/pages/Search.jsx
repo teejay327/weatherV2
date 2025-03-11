017e661e3308dc3110c04acea922d423f8780334
teejay327 <mrenergy@hotmail.com>
2025-03-11 23:56:55 +1000

    Added Search input form

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,9 +3,19 @@
 const Search = () => {
 
   return (
-
-    <h1 className="text-stone-200">Search</h1>
+    <form >
+      <div>
+        <label>Search for a location</label>
+        <input type="text" placeholder="Gold Coast" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+    
   )
 };
 
 export default Search;
+
+<h1 className="text-stone-200">Search</h1>
