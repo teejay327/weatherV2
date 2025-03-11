be84cfd4f5f337d19b50ad2abcd6b5f587dae8a0
teejay327 <mrenergy@hotmail.com>
2025-03-12 00:20:28 +1000

    Improved layout input form

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,12 +4,12 @@ const Search = () => {
 
   return (
     <form >
-      <div>
-        <label>Search for a location</label>
-        <input type="text" placeholder="Gold Coast" />
+      <div className="flex gap-4 mb-1">
+        <label className="text-stone-200 font-bold p-2">Search for a location</label>
+        <input type="text" placeholder="Gold Coast" className="p-2 rounded-md"/>
       </div>
       <div>
-        <button type="submit">Submit</button>
+        <button type="submit" className="text-stone-200 font-bold bg-stone-900 rounded-md px-6 py-2 hover:bg-slate-800 cursor-pointer">Submit</button>
       </div>
     </form>
     
