bdef30fb088e83c126b045a0b0c8dcaecce645b1
teejay327 <mrenergy@hotmail.com>
2025-03-31 22:25:14 +1000

    Hide/unhide sidebar when hocering to left on small devices

--- src/components/UI/Sidebar.jsx
+++ src/components/UI/Sidebar.jsx
@@ -1,10 +1,15 @@
-
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen ] = useState(false);
 
   return (
-    <aside className="w-1/4 h-full px-2 py-4 bg-stone-900 md:w-72 text-stone-300 rounded-r-md">
-      <div>
+    <aside className={`w-1/4 h-full px-2 py-4 bg-stone-900 md:w-72 text-stone-300 rounded-r-md
+      transition-all duration-300 ease-in-out ${isOpen ? "w-64 px-4" : "w-[4px] overflow-hidden" } md:w-72`}
+     onMouseEnter={() => setIsOpen(true)}
+     onMouseLeave={() => setIsOpen(false)}
+    >
+      <div className={`${isOpen ? "opacity-100" : "opacity-0 md:opacity-100"} transition-opacity duration-300`}>
         <p>Weather</p>
       </div>
       <h2>Weatherzone</h2>
