e80e593e1d7479bf7b4d207ce377b0c6c3ce6030
teejay327 <mrenergy@hotmail.com>
2025-04-20 22:45:59 +1000

    Finalised Login page validation checks

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,12 +16,49 @@ const Login = () => {
   );
 
   const submitHandler = (event) => {
-   event.preventDefault();
-   console.log('LOGIN FORM DATA:', formState.inputs);
+    event.preventDefault();
+    console.log('LOGIN FORM DATA:', formState.inputs);
   }
 
   return (
-    <div className="p-4 bg-gray-900 text-white max-w-md mx-auto mt-10">
+    <Card className="bg-gray-900 p-6 max-w-md mx-auto mt-10 text-white">
+      <h2 className="text-xl font-bold mb-2">Login required</h2>
+      <hr className="border-stone-600 mb-4"/>
+      <form onSubmit={ submitHandler } className="space-y-4">
+        <Input 
+           id="email" 
+           element="input" 
+           type="email" 
+           label="Email" 
+           validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]} // changed type from text to email
+           errorText="Please enter a valid email address" 
+           onInput={ inputHandler } 
+           placeholder="yourname@example.com"
+        />
+
+         <Input 
+           id="password" 
+           element="input" 
+           type="password" 
+           label="Password" 
+           validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]} // changed type from text to email
+           errorText="Please enter a valid password" 
+           onInput={ inputHandler } 
+           placeholder="your password"
+        />
+
+        <button type="submit" 
+           disabled={!formState.isValid} 
+           className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 
+            text-white rounded disabled:opacity-50">
+          Login
+        </button>
+      </form>
+    </Card >
+  );
+
+
+    // <div className="p-4 bg-gray-900 text-white max-w-md mx-auto mt-10">
       
       {/* <h2 className="text-xl font-bold mb-4">login</h2>
       <form>
@@ -36,26 +73,11 @@ const Login = () => {
           placeholder="yourname@example.com"
         />
       </form> */}
-    </div>
-  )
+    // </div>
+  // )
 
 
-//   return (
-//     <Card className="login">
-//       <h2 className="text-xl font-bold mb-2">Login required</h2>
-//       <hr />
-//       <form onSubmit={ submitHandler } className="space-y-4">
-//         <Input id="email" element="input" type="email" label="Email" 
-//           validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]} // changed type from text to email
-//           errorText="Please enter a valid email address" onInput={ inputHandler } placeholder="yourname@example.com"
-//         />
-//         <button type="submit" disabled={!formState.isValid} className="bg-blue-600 hover:bg-blue-700 
-//           px-4 py-2 text-white rounded disabled:opacity-50">
-//           Submit
-//         </button>
-//       </form>
-//     </Card >
-//   );
+
 };
 
 export default Login;
