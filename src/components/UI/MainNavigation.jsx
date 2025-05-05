f3d7c883e1e578c3a3c9d36629a1329080691a3e
teejay327 <mrenergy@hotmail.com>
2025-05-05 23:53:20 +1000

    Made final changes to links in MainNavigation

--- src/components/UI/MainNavigation.jsx
+++ src/components/UI/MainNavigation.jsx
@@ -23,9 +23,6 @@ const MainNavigation = ({ isMobile = false, closeMenu }) => {
           <li className="bg-stone-900 text-slate-200 px-4 py-2 rounded-md hover:text-yellow-700">
             <NavLink 
               to="/" 
-              // className={({isActive}) => 
-              //   !showCharts && isActive ? "text-yellow-400 hover:bg-slate-700" : undefined
-              // } 
               className={({isActive}) => 
                 isActive && !activeLink ? "text-yellow-400 hover:bg-slate-700" : "hover:bg-slate-700"                
               }
