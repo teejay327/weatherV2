9f5222964c9b52ff42a3c7cda45d813016bfd45a
teejay327 <mrenergy@hotmail.com>
2025-06-10 23:49:04 +1000

    Edited WindyRadar page

--- src/components/shared/FormElements/WindyRadar.jsx
+++ src/components/shared/FormElements/WindyRadar.jsx
@@ -1,9 +1,14 @@
 const  WindyRadar = () => {
 
-<div>
+<div className="border-spacing-0">
   <iframe
+    title="Windy radar"
+    width="100%"
+    height="500"
+    src="https://embed.windy.com/embed2.html?lat=-27.4705&lon=153.026&detailLat=-27.4705&detailLon=153.026&width=650&height=500&zoom=8&level=surface&overlay=rain&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1"
     
-  />
+    allowFullScreen
+ ></iframe>
 </div>
 }
 
