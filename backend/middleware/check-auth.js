066698a4bb92046cf9b9644bec9f352f8264de3b
teejay327 <mrenergy@hotmail.com>
2025-07-27 23:50:56 +1000

    Middleware

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const checkAuth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Authentication failed. Token missing or invalid'});
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.userData = { userId: decodedToken.userId, email: decodedToken.email };
+    next();
+  } catch (err) {
+    console.error('[JWT ERROR]', err.message);
+    res.status(403).json({ message: 'Authentication failed. Token could not be verified' });
+  }
+}
+
+export default checkAuth;
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users-controller');
+const checkAuth = require('../middleware/check-auth');
 
+// Public routes
 router.post('/signup', usersController.signup);
 router.post('/login', usersController.login);
+
+// Protected routes
+router.get('protected', checkAuth, (req,res) => {
+  res.json({ message: `Ahoy, ${req.user.email}! You've reached the secure harbour!` });
+});
 
 module.exports = router;
