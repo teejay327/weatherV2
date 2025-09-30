ca1267e0803a9efb4583f9446a9148e60d0fec68
teejay327 <mrenergy@hotmail.com>
2025-09-30 23:44:03 +1000

    Refactored users-controller for better signup procedure with jwt token

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -5,77 +5,67 @@ import User from '../models/user.js';
 const signup = async(req,res) => {
   const { email,password } = req.body;
 
-  let existingUser;
   try {
-    existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(422).json({ message: 'User already exists' });
     }
-  } catch(err) {
-    return res.status(500).json({ message: 'Signing up failed' });
-  }
+    const hashedPassword = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        password: hashedPassword
+    });
 
-  let hashedPassword;
-  try {
-    hashedPassword = await bcrypt.hash(password, 12);
-  } catch(err) {
-    return res.status(500).json({ message: 'Could not create user' });
-  }
-
-  const newUser = new User({
-    email,
-    password: hashedPassword
-  });
-
-  try {
     await newUser.save();
-  } catch(err) {
-    return res.status(500).json({ message: 'Signup failed while saving' })
-  }
 
-  res.status(201).json({ userId: newUser.id, email: newUser.email });
+    // generate jwt for auto-login
+    const token = jwt.sign(
+      {userId: newUser.id, email: newUser.email},
+      process.env.JWT_SECRET,
+      {expiresIn: '1d'}
+    );
+
+    // respond with user details and token
+    res.status(201).json({
+      userId: newUser.id,
+      email: newUser.email,
+      token
+    });
+  } catch(err) {
+    console.error({message: 'Signup failed', detail: err.message});
+  }
 }
 
 //Login controller
 const login = async(req, res) => {
   const { email, password } = req.body;
 
-  let existingUser;
   try {
-    existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
     if (!existingUser) {
       return res.status(403).json({ message: 'Invalid credentials'});
     }
-  } catch (err) {
-    return res.status(500).json({ message: 'Login failed'});
-  }
 
-  let isValidPassword = false;
-  try {
-    isValidPassword = await bcrypt.compare(password, existingUser.password);
+    const isValidPassword = await bcrypt.compare(password, existingUser.password);
     if (!isValidPassword) {
       return res.status(403).json({ message: 'Invalid email or password'});
     }
-  } catch (err) {
-      return res.status(500).json({ message: 'Could not log you in'});
-  }
-  
-  let token;
-  try {
-    token = jwt.sign(
+
+    const token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
       process.env.JWT_SECRET,
       { expiresIn: '1d' }
     );
-  } catch (err) {
-    return res.status(500).json({ message: 'Login failed: Could not generate token'});
-  }
 
-  res.status(200).json({
-    userId: existingUser.id,
-    email: existingUser.email,
-    token
-  });
+    res.status(200).json({
+      userId: existingUser.id,
+      email: existingUser.email,
+      token
+    });
+  } catch(err) {
+    console.error('[Login] error:', err);
+    res.status(500).json({message: 'Login failed', detail: err.message});
+  }
 };
 
 export default { signup, login };
