a04c6f551ab351c336f88071874f1a3130fd124f
teejay327 <mrenergy@hotmail.com>
2025-11-15 22:47:57 +1000

    Finished MiniNightMap.jsx

--- src/components/maps/MiniNightMap.jsx
+++ src/components/maps/MiniNightMap.jsx
@@ -1,23 +1,39 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-const MiniNightMap = ({ locations }) => {
-  if (!locations || typeof locations.lat !== "number" || typeof locations.lon !== "number" ) {
+const glowingDotIcon = L.divIcon({
+  className: "mini-map-dot",
+  html: '<div class="mini-map-dot-inner"></div>',
+  iconSize: [16,16],
+  iconAnchor: [8, 8]
+})
+
+const MiniNightMap = ({ location }) => {
+  if (!location || typeof location.lat !== "number" || typeof location.lon !== "number" ) {
     return null;
   }
 
   // Centre the map on the first saved location
-  const centre = locations[0];
+  const centre = [location.lat, location.lon];
 
   return (
     <div className="mt-2 h-40 w-full rounded-lg overflow-hidden border border-slate-700 bg-slate-900/80 shadow-sm">
-      <MapContainer>
-        <TileLayer>
-
-        </TileLayer>
-        <Marker>
-
-        </Marker>
+      <MapContainer
+        center={centre}
+        zoom={7}
+        scrollWheelZoom={false}
+        dragging={false}
+        doubleClickZoom={false}
+        zoomControl={false}
+        style={{ width: "100%", height: "100&"}}
+      >
+        {/* Dark earth basemap */}
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}/{r}.png"
+          attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+        />
+        <Marker position={center} icon={glowingDotIcon} />
       </MapContainer>
     </div>
   )
